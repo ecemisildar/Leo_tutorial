c43609770439762d7e0b3805f7786b46ebd03c62
Ecem <ecemisildar@gmail.com>
2026-04-14 17:09:16 +0200

    update teleop

--- leo_teleop/setup.py
+++ leo_teleop/setup.py
@@ -1,17 +1,21 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'leo_teleop'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='user@example.com',
-    description='Basic teleop node for Leo Rover',
-    license='Apache License 2.0',
+    maintainer='ecem',
+    maintainer_email='you@example.com',
+    description='Basic Leo Rover teleop package',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'leo_teleop_node = leo_teleop.leo_teleop_node:main',
